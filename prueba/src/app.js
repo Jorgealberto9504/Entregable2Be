71c87f78e957957ed5bad8ba1a33935b1be865fa
Jorge Escalante <jorgealberto9504@gmail.com>
2024-09-17 15:48:11 -0600

    entregable2

--- prueba/src/app.js
+++ prueba/src/app.js
@@ -3,14 +3,11 @@ import productsRouter from "./routes/products.js";
 import cartsRouter from "./routes/carts.js"
 
 const app = express()
-
 app.use (express.json())
+
 app.use('/static', express.static('public')) 
 
-
 app.use('/api/products', productsRouter) 
-
-
 app.use('/api/carts', cartsRouter)
 
 app.get('/', (req,res)=>{
